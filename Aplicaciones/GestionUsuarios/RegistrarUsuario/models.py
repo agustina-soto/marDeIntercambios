9f02cc3a6dabd7eea303c41ac414ccca806be244
Lucky <lucas.beliz@hotmail.com>
2024-05-12 18:47:19 -0300

    VALIDAME

--- Aplicaciones/GestionUsuarios/RegistrarUsuario/models.py
+++ Aplicaciones/GestionUsuarios/RegistrarUsuario/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Usuario(models.Model):
-    nombre = models.charField(max_length = 100)
+    nombre = models.charField(max_length = 30)
     
 # Create your models here.
