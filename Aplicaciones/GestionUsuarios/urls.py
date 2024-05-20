53e2c57af8d96ef10e5c521d0ca62178de443888
Agustina Soto <agustinasoto239@gmail.com>
2024-05-20 15:01:25 -0300

    Elimino una urls que estaba de mas (la de editar_oublicacion en gestion de usuarios)

--- Aplicaciones/GestionUsuarios/urls.py
+++ Aplicaciones/GestionUsuarios/urls.py
@@ -5,7 +5,6 @@ from .VerPerfilUsuario import views as profile_views
 urlpatterns = [
     path('registrar_usuario/', register_views.registro, name='registrar_usuario'),
     path('exitoRegistro/', register_views.exito_registro, name='exito_registro'),
-    path('<int:publicacion_id>/editar/', profile_views.render, name='editar_publicacion'), # PENDIENTE
     path('perfilDeUsuario/', profile_views.perfil_view, name='perfil_de_usuario' )
 ]
 
