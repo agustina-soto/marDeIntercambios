f4e86dbf715cf0efefbe559d53a9b4280f4df86d
Lucky <lucas.beliz@hotmail.com>
2024-05-21 13:30:45 -0300

    Agrego diseño de VerPerfilDeUsuario

--- Aplicaciones/GestionUsuarios/VerPerfilUsuario/views.py
+++ Aplicaciones/GestionUsuarios/VerPerfilUsuario/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Creando los modelos de ver
 def perfil_view(request):
-    return render(request, 'perfilDeUsuario.html')
+    usuario = request.user
+    nombreDeUsuario = usuario.username
+    fechaDeNacimiento = usuario.fecha_nacimiento
+    dniDeUsuario = usuario.dni
+    return render(request, 'perfilDeUsuario.html',{ #acá agrego el arreglo de publicaciones
+        'username': nombreDeUsuario, 
+        'fechaNacimiento': fechaDeNacimiento,
+        'dni': dniDeUsuario
+        }
+    )
