d954b00f5d303888fe6692f8437b8b53daf608e5
Gio Scalmati <gioscalmati@hotmail.com>
2024-06-25 15:52:48 -0300

    Sumo listas de usuarios registrados y baneados, inicio creacion de usuarios adimn

--- Aplicaciones/GestionUsuarios/RegistrarUsuario/views.py
+++ Aplicaciones/GestionUsuarios/RegistrarUsuario/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegistroForm
 from .models import *
+from MDI.decorator import login_required, user_passes_test_superuser
 
 def registro(request):
     if request.method == 'POST':
@@ -17,3 +18,21 @@ def registro(request):
     else:
         form = RegistroForm()
     return render(request, 'GestionUsuarios/registro.html', {'form': form})
+
+
+@login_required 
+@user_passes_test_superuser
+def registrar_usuario_administrador(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST) #Se verifcan datos dentro del form y se genera instancia de un usuario para su registro
+        if form.is_valid(): #si no ocurrió ningún error...
+            try:
+                form.save()
+                #usuario = form.save() 
+                #login(request, usuario) #No sé si hace falta loguear al usuario que se registre, la h.u no lo especifica
+                messages.success(request, "Usuario Administrador Registrado con éxito!")
+            except Exception as e:
+                form.add_error(None, str(e))
+    else:
+        form = RegistroForm()
+    return render(request, 'GestionUsuarios/registro_administrador.html', {'form': form})
--- Aplicaciones/GestionUsuarios/urls.py
+++ Aplicaciones/GestionUsuarios/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .RegistrarUsuario import views as register_views
 from .VerPerfilUsuario import views as profile_views
+from .VerListaUsuarios import views as lista_views
 
 urlpatterns = [
     path('registrar_usuario/', register_views.registro, name='registrar_usuario'),
     path('perfilDeUsuario/', profile_views.perfil_view, name='perfil_de_usuario' ),
     path('perfilDeUsuario/editar/', profile_views.editar_perfil_view, name='editar_perfil_usuario'),
+    path('ver_lista_usuarios/', lista_views.ver_lista_usuarios, name='ver_lista_usuarios'),
+    path('ver_lista_usuarios_baneados/', lista_views.ver_lista_usuarios_baneados, name='ver_lista_usuarios_baneados'),
+    path('registrar_usuario_administrador/', register_views.registrar_usuario_administrador, name='registrar_usuario_administrador'),
 ]
 
