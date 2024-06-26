a61476dbe1eb478365d1f459e76054668f8a34b9
Lucky <lucas.beliz@hotmail.com>
2024-06-26 12:52:16 -0300

    Agrego Recuperar Contraseña

--- Aplicaciones/GestionUsuarios/urls.py
+++ Aplicaciones/GestionUsuarios/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .RegistrarUsuario import views as register_views
 from .VerPerfilUsuario import views as profile_views
 from .ActualizarContraseña import views as password_update
-
+from .RecuperarContraseña import views as password_recovery
 urlpatterns = [
-    path('registrar_usuario/', register_views.registro, name='registrar_usuario'),
+    path('registrarUsuario/', register_views.registro, name='registrar_usuario'),
+    path('recuperarPassword', password_recovery.recuperar_contraseña, name='recuperar_password'),
     path('perfilDeUsuario/', profile_views.perfil_view, name='perfil_de_usuario' ),
     path('perfilDeUsuario/editar/', profile_views.editar_perfil_view, name='editar_perfil_usuario'),
     path('perfilDeUsuario/actualizarPassword', password_update.actualizar_password, name='actualizar_password'),
