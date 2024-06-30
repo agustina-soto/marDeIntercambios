9ea5d1b9020106920900bb6171ce4d1b40a40205
Lucky <lucas.beliz@hotmail.com>
2024-06-30 15:42:05 -0300

    arreglo mis urls a formato estandar y modifico url de perfil_de_usuario

--- Aplicaciones/GestionUsuarios/VerPerfilUsuario/views.py
+++ Aplicaciones/GestionUsuarios/VerPerfilUsuario/views.py
@@ -1,18 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from Aplicaciones.GestionUsuarios.VerPerfilUsuario.forms import EditarPerfilForm
-from Aplicaciones.Modelos.models import Oferta, Publicacion
+from Aplicaciones.Modelos.models import Oferta, Publicacion, Usuario
 from MDI.decorator import login_required
 from django.contrib.auth.decorators import login_required # por que hay 2 login required importados??
 
 @login_required 
-def perfil_view(request):
-    #Podría haber mandado un usuario completo, pero no lo contemplé, después lo arreglo
-    usuario = request.user
+def perfil_view(request, user_id):
+   
+    if request.user.id != user_id:
+        return HttpResponse('No tienes permiso para ver el perfil de otro usuario.', status=403)
+    
+    usuario = Usuario.objects.get(id=user_id)
     correo = usuario.username
     fechaDN = usuario.fecha_nacimiento
     dni = usuario.dni
-    #El ver perfil de la h.u estaba raro, por el momento agregé para que un usuario autenticado vea su perfil
+
     pubs = Publicacion.objects.filter(autor=request.user).prefetch_related('fotos') 
     ofertas = Oferta.objects.filter(autor=request.user).prefetch_related('fotos')
     return render(request, 'GestionUsuarios/perfilDeUsuario.html',
@@ -24,14 +28,14 @@ def perfil_view(request):
     )
 
 @login_required
-def editar_perfil_view(request):
+def editar_perfil_view(request, user_id):
     usuario = request.user
     if request.method == 'POST':
         form = EditarPerfilForm(request.POST, instance=usuario)
         if form.is_valid():
             form.save()
             messages.success(request, 'Datos personales actualizados')
-            return redirect('perfil_de_usuario')
+            return redirect('perfil_de_usuario/', user_id)
     else:
         form = EditarPerfilForm(instance=usuario)
     
