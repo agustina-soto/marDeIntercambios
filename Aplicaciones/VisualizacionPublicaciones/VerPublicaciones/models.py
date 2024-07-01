ddce53f4d9db8cfea08e5a7b4b889bb84d48d7a8
Agustina Soto <agustinasoto239@gmail.com>
2024-07-01 04:23:00 -0300

    Saque algun login required que estaba de mas

---
+++ Aplicaciones/VisualizacionPublicaciones/VerPublicaciones/models.py
@@ -0,0 +1,3 @@
+from Aplicaciones.Modelos.models import Publicacion
+
+# Create your models here.
--- MDI/views.py
+++ MDI/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
-from Aplicaciones.Modelos.models import Publicacion
-from django.core.paginator import Paginator
 
 def home(request):
-    # Obtiene todas las publicaciones excluyendo las eliminadas
-    dataPublicaciones = Publicacion.objects.exclude(estado='eliminada')
-
-    # Pagina los resultados
-    resultados_paginados = Paginator(dataPublicaciones, 10)
-    page_number = request.GET.get("page")
-    page_obj = resultados_paginados.get_page(page_number if page_number else 1)
-
-    # Obtiene las publicaciones seleccionadas de la sesión
-    publicaciones_seleccionadas = request.session.get('publicaciones_a_comparar', [])
-
-    # Pasa las publicaciones y las publicaciones seleccionadas al contexto
+    
+    usuario_actual = request.user
+    
     context = {
-        'publicaciones': page_obj,
-        'publicaciones_a_comparar': publicaciones_seleccionadas,
+        'usuario': usuario_actual,
+        # no se qué pasar
     }
 
     return render(request, 'home.html', context)
