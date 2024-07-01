6a353b80e17bd51b675a0a3efff3eeeefb3fb23f
Agustina Soto <agustinasoto239@gmail.com>
2024-06-30 23:09:31 -0300

    Modifico una migracion que me olvide de completar (le toque algo para que me deje hacer una cosa y me olvide de completarlo despues)

--- Aplicaciones/Modelos/migrations/0007_intercambios.py
+++ Aplicaciones/Modelos/migrations/0007_intercambios.py
@@ -10,3 +10,14 @@ class Migration(migrations.Migration):
         ('Modelos', '0006_alter_oferta_estado_alter_oferta_precio_estimado_and_more'),
     ]
 
+    operations = [
+        migrations.CreateModel(
+            name='Intercambios',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('estado', models.CharField(choices=[('pendiente', 'Pendiente'), ('aceptado', 'Aceptado'), ('rechazado', 'Rechazado')], default='aceptado', max_length=10)),
+                ('fecha_aceptacion', models.DateTimeField(null=True)),
+                ('publicacion', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='intercambios', to='Modelos.publicacion')),
+            ],
+        ),
+    ]
